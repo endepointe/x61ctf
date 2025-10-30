99aa123778ca7b189a7d7e45cebeed869ffa7e47
endepointe <endepointe@gmail.com>
2025-10-29 21:40:16 -0700

    added d1 worker

--- site/workers/index.ts
+++ site/workers/index.ts
@@ -6,6 +6,11 @@ export default {
   async fetch(request, env): Promise<Response> {
     const { pathname } = new URL(request.url);
     if (pathname === "/challenges") {
+      const { results } = await env.x61ctf_d1_db.prepare(
+        "SELECT * FROM Customers WHERE CompanyName = ?",
+      )
+      .bind("Bs Beverages")
+      .run();
       return Response.json(results);
     }
     return null;
