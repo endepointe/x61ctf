3f3a307c7b99585b0eb8e7f22af4c05573354f60
endepointe <endepointe@gmail.com>
2026-01-07 22:32:24 -0800

    saltandpeppernext

--- containers/postgres/init.sql
+++ containers/postgres/init.sql
@@ -1,8 +1,16 @@
+create schema x61ctf authorization x61ctf;
+
+set search_path to x61ctf,public;
+
+create table if not exists seasoning (
+  salt text not null,
+  -- store the pepper in cupboard
+);
 
 create table if not exists users (
-  id serial primary key,
-  name varchar(50),
-  age integer
+  user_id serial primary key,
+  username varchar(50) not null,
+  password_hash text
 );
 
 insert into users (name,age) values
